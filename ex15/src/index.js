66cb94e412ea3bcc7f21a3870f2184028eef234e
Calculator-dev <haris_aksalic@hotmail.com>
2021-03-17 09:11:18 +0100

    Core changes

--- ex15/src/index.js
+++ ex15/src/index.js
@@ -1,13 +1,17 @@
-
 function myDoWhile () {
     
     var myNumbers = "";
     
     var i = 0;
     do {
-        myNumbers += i + ", ";
+      
+        myNumbers += i;
+        if (i < 9){
+        myNumbers += ", ";
+      }
         i++;
     }while (i < 10)
+    
     console.log(myNumbers);
 }
 myDoWhile();
